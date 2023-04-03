c794c99464b7f22a8ada193cf24479b6b7b51726
Antonio-Carvalho-1 <i4t59r6bvwf5icdya@hotmail.com>
2023-04-03 07:58:43 -0300

    Atualização

---
+++ Python-testes/Home.py
@@ -0,0 +1,14 @@
+import webbrowser
+
+url = 'https://www.pciconcursos.com.br/concursos/'
+url2 = 'https://noticiasconcursos.com.br/concursos-previstos/'
+url3 = 'https://blog.grancursosonline.com.br/ultimas-noticias/'
+url4 = 'https://www.estrategiaconcursos.com.br/blog/concursos-fiscais/'
+url5 = 'https://blog.grancursosonline.com.br/concursos-fiscais/'
+
+
+webbrowser.open(url)
+webbrowser.open_new_tab(url2)
+webbrowser.open_new_tab(url3)
+webbrowser.open_new_tab(url4)
+webbrowser.open_new_tab(url5)
---
+++ Python-testes/Teste grafico.py
@@ -0,0 +1,16 @@
+import tkinter as tk
+import random
+import time
+
+# Criar a janela
+janela = tk.Tk()
+
+# Adicionar um título
+janela.title("Show do Milhão")
+
+# Definir as dimensões
+janela.geometry("1280x720")
+
+# Executar o loop da janela
+janela.mainloop()
+
